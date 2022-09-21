b711cf16500770ca1e62f71d04c87db8e1a531ae
romantut1988 <romantut1988@mail.ru>
2022-09-22 02:05:16 +0300

    jQuery tabs added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
-$(function() {
+$(function () {
 	$('.works-carousel').owlCarousel({
-	    loop:true,
-	    margin:30,
-	    nav:true,
-	    dots: false,
-	    navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
-	    startPosition: 1,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+		startPosition: 1,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	});
+
+	$('ul.tabs__caption').on('click', 'li:not(.active)', function (e) {
+		e.preventDefault();
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 	});
 });
