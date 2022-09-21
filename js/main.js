2b937e7f6be5f4d30c32b1d3d73d1716f8bf0a0a
romantut1988 <romantut1988@mail.ru>
2022-09-21 15:07:58 +0300

    jquery library connection

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('.gallery').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
+});
