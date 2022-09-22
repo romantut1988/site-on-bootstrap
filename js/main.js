62879a24fc72b51e399cc51cdbca75ea24ad5213
romantut1988 <romantut1988@mail.ru>
2022-09-22 10:31:49 +0300

    section testimonials

--- js/main.js
+++ js/main.js
@@ -19,6 +19,26 @@ $(function () {
 		}
 	});
 
+	$('.contributor-carousel').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: true,
+		dots: false,
+		navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+		startPosition: 1,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	});
+
 	$('ul.tabs__caption').on('click', 'li:not(.active)', function (e) {
 		e.preventDefault();
 		$(this)
